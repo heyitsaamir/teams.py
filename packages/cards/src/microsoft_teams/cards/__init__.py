a5e53dfa9224b3826b66e5934b3741394ebc4b5c
Corina Gum <14900841+corinagum@users.noreply.github.com>
2026-03-30 16:25:27 -0700

    Remove deprecated action wrappers

--- packages/cards/src/microsoft_teams/cards/__init__.py
+++ packages/cards/src/microsoft_teams/cards/__init__.py
@@ -5,12 +5,6 @@ Licensed under the MIT License.
 
 import logging
 
-from . import actions
-from .actions import *  # noqa: F403
 from .core import *
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-# Combine all exports from submodules
-__all__: list[str] = []
-__all__.extend(actions.__all__)
